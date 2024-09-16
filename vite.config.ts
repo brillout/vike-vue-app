8a23f3695d783a17bf08b5fcefa3a842196c8b26
Bati <no-reply@batijs.dev>
2024-09-16 21:15:31 +0200

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import md from "unplugin-vue-markdown/vite";
+import vue from "@vitejs/plugin-vue";
+import { telefunc } from "telefunc/vite";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [
+    vike({}),
+    devServer({
+      entry: "hono-entry.ts",
+
+      exclude: [
+        /^\/@.+$/,
+        /.*\.(ts|tsx|vue)($|\?)/,
+        /.*\.(s?css|less)($|\?)/,
+        /^\/favicon\.ico$/,
+        /.*\.(svg|png)($|\?)/,
+        /^\/(public|assets|static)\/.+/,
+        /^\/node_modules\/.*/,
+      ],
+
+      injectClientScript: false,
+    }),
+    telefunc(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    md({}),
+  ],
+});
